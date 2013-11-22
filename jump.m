ee926fc22405d47e94c3acadf70c4ab1709cfd6d
Junfeng Li <autozimu@gmail.com>
2013-11-21 21:41:15 -0500

    jump: display info on non-unix platform

--- jump.m
+++ jump.m
@@ -5,7 +5,11 @@ function dst = jump(dsttag)
 %   - dsttag: string, part of the directory name to identify destination path
 
 % Depend on `autojump`
-cmd = ['/usr/local/bin/autojump ', dsttag];
+if ~isunix()
+    disp('Not implemented yet non-unix platform');
+    return
+end
+cmd = ['/usr/bin/env autojump ', dsttag];
 [status, dst] = system(cmd);
 if status == 0
     cd(dst(1:end-1))
