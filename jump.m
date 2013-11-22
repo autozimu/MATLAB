1736f7c777d82f03974b2742507209a2e4db804e
Junfeng Li <autozimu@gmail.com>
2013-11-22 15:37:16 -0500

    implement jump on windows

--- jump.m
+++ jump.m
@@ -1,16 +1,22 @@
 function dst = jump(dsttag)
 % JUMP smart cd
-% 
+%
 % Args:
 %   - dsttag: string, part of the directory name to identify destination path
 
 % Depend on `autojump`
-if ~isunix()
-    disp('Not implemented yet non-unix platform');
-    return
+if isunix()
+    cmd = ['/usr/bin/env autojump ', dsttag];
+else
+    cmd = ['C:\cygwin64\bin\python2.7.exe /usr/bin/autojump ', dsttag];
 end
-cmd = ['/usr/bin/env autojump ', dsttag];
 [status, dst] = system(cmd);
 if status == 0
+    if ~isunix()
+        % translate cygwin path to windows
+        [~, dst] = system(['C:\cygwin65\bin\cgypath.exe --windows ', dst]);
+    end
     cd(dst(1:end-1))
+else
+    disp(dst);
 end
