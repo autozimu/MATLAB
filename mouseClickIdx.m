f7172fb17c0d97458fb367cae8fdac28e2c57bf4
Junfeng Li <autozimu@gmail.com>
2014-02-28 21:39:20 -0500

    fix last commit

--- mouseClickIdx.m
+++ mouseClickIdx.m
@@ -1,6 +1,6 @@
 function mouseClickIdx(X, Y)
-    X = [1:10];
-    Y = rand(10, 1)';
+%     X = [1:10];
+%     Y = rand(10, 1)';
     h = plot(X, Y, 'o-');
     set(h, 'ButtonDownFcn', @buttonDownCallback);
     
