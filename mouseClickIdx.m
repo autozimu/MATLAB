d227fe5f2cc28ca2d08b577db4895c3eda8bee8e
Junfeng Li <autozimu@gmail.com>
2014-02-28 21:30:57 -0500

    add script mouseclickidx
    click to get index of the point

---
+++ mouseClickIdx.m
@@ -0,0 +1,13 @@
+function mouseClickIdx(X, Y)
+    X = [1:10];
+    Y = rand(10, 1)';
+    h = plot(X, Y, 'o-');
+    set(h, 'ButtonDownFcn', @buttonDownCallback);
+    
+    function buttonDownCallback(o, e)
+        p = get(gca, 'CurrentPoint'); % get current point
+        p = p(1, 1:2);
+        [~, idx] = min((X - p(1)).^2 + (Y - p(2)).^2);
+        title(sprintf('%g', idx));
+    end
+end
