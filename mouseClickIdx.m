8d0d097d0ba2c0ecf7569cc33c8f1788d7d05662
Junfeng Li <autozimu@gmail.com>
2014-03-04 16:55:08 -0500

    create new figure before plot

--- mouseClickIdx.m
+++ mouseClickIdx.m
@@ -1,9 +1,10 @@
 function mouseClickIdx(X, Y)
 %     X = [1:10];
 %     Y = rand(10, 1)';
+    figure;
     h = plot(X, Y, 'o-');
     set(h, 'ButtonDownFcn', @buttonDownCallback);
-    
+
     function buttonDownCallback(o, e)
         p = get(gca, 'CurrentPoint'); % get current point
         p = p(1, 1:2);
